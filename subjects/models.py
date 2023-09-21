1f767266db9dcff5f8c4e204df8c384470800448
Chalisa Thummeraj <6410685041@cn103>
2023-09-21 16:43:18 +0700

    Add class subject

--- subjects/models.py
+++ subjects/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Subject(models.Model):
+    name = models.CharField(max_length=64)
+    code = models.CharField(max_length=6)
+    professor = models.CharField(max_length=128)
+    seat = models.IntegerField()
