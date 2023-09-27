6088191988b18e451aa443b4396a0b2bd967a8d3
Siriphatson Sringamphong <6410685249@cn330>
2023-09-27 22:29:57 +0700

    add class Subject

--- users/models.py
+++ users/models.py
@@ -1,11 +1,18 @@
 from django.db import models
-from ..subjects.models import Subject
-
 # Create your models here.
 
+class Subject(models.Model):
+    name = models.CharField(max_length=64)
+    code = models.CharField(max_length=6)
+    professor = models.CharField(max_length=128)
+    quota = models.IntegerField(default=0)
+    seat = models.IntegerField()
+    
 class Student(models.Model):
     name = models.CharField(max_length=64)
     surname = models.CharField(max_length=64)
-    id = models.IntegerField()
+    id = models.IntegerField(primary_key=True)
     status = models.CharField(max_length=20)
-    subjects = models.ManyToManyField(Subject, blank=True, related_name="list Subjects")
+    # subjects = models.ManyToManyField(Subject, blank=True, related_name="list Subjects")
+
+# class 
